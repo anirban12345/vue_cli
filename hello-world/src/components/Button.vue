f122ad6cfed52ff9f55442eb1489104a404b1068
anirban12345 <you@example.com>
2022-02-23 19:24:48 +0530

    .

--- hello-world/src/components/Button.vue
+++ hello-world/src/components/Button.vue
@@ -9,10 +9,26 @@ export default {
     text: String,
     classname: String
   },
+  data(){
+        return {
+            text:this.text,
+            classname:this.classname
+        }
+  }, 
   methods:{
-    onCLick()
+    async onCLick()
     {
       console.log(this.text);
+      if(this.text=="Activate")
+      {
+        this.text="Deactivate";
+        this.classname="btn btn-danger btn-sm";
+      }
+      if(this.text=="Deactivate")
+      {
+        this.text="Activate";
+        this.classname="btn btn-success btn-sm";
+      }
     }
   },
 }
@@ -20,5 +36,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css');
 </style>
--- hello-world/src/components/Pdata.vue
+++ hello-world/src/components/Pdata.vue
@@ -2,23 +2,33 @@
       <tr :class="[pdata.active ? 'alert-success' : 'alert-danger']">
         <td>{{pdata.name}}</td>
         <td>{{pdata.address}}</td>
+        <td><Button :class="classname" :text="text" /></td>
       </tr>
 </template>
 
 <script>
 
-
+import Button from './Button.vue'
 export default {
   name: 'Pdata',  
-  
+  components: {     
+    Button    
+  },
   props: {
+    text: String,
+    classname: String,
     pdata: Object,
   },
-  
+  data(){
+        return {
+            text:"Activate",
+            classname:'btn btn-danger btn-sm'            
+        }
+  },  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css');
 </style>
